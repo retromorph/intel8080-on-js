1ac0b427c53c4f3e48670d403050a745e25ee3b3
Increative <awesome.commerce@yandex.ru>
2019-09-30 03:24:36 +0300

    RAM item

---
+++ intel8080/ram.js
@@ -0,0 +1,41 @@
+let RAMItem = function () {
+    let priv = {
+        val: 0b00000000
+    };
+
+    this.setBinary = (mask) => {
+        priv.val |= mask;
+    };
+
+    this.set = (val) => {
+        priv.val = val;
+    };
+
+    this.get = () => {
+        return priv.val;
+    };
+
+    this.toString = () => {
+        let result = "";
+        for (let i = 0, mask = 0b00000001; i < 8; i++) {
+            //console.log(((priv.val & mask) >> i+1));
+            result += ((priv.val & mask) >> i).toString();
+            mask <<= 1;
+        }
+        return result.split("").reverse().join("");
+    }
+
+};
+
+export default {
+    RAM: {
+        init() {
+            for (let i = 0b00000000; i < 0b11111111; i += 0b00000001) {
+                this[i] = new RAMItem();
+            }
+        },
+        get(address) {
+            return this[address];
+        }
+    }
+}
